be4e05e5aef7a1a43e03d0d9c2076d532de30963
SLey3 <ghub4127@gmail.com>
2022-10-04 07:57:14 -0700

    added score system + removed unecessary code

--- Wordle.py
+++ Wordle.py
@@ -4,7 +4,8 @@
 """Wordle main file"""
 
 from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
-from utils import choose_word, validate_responce
+from utils import choose_word, validate_responce, ScoreFileParser
+from time import sleep
 
 
 def wordle():
@@ -12,26 +13,27 @@ def wordle():
 
     answer = choose_word()
 
+
     res_initial = []
 
     def enter_action():
-        MN = 5
-        for i in range(5):
-            res_initial.append(gw.get_square_letter(gw.get_current_row(), N_COLS-MN))
-            MN -= 1
-        MN += 5
+        for i in range(N_COLS):
+            res_initial.append(gw.get_square_letter(gw.get_current_row(), i))
         res = "".join(res_initial).lower()
-        result, not_word = validate_responce(gw, res, answer)
+        result, not_word, tempfile = validate_responce(gw, res, answer)
         res_initial.clear()
+        score = ScoreFileParser(gw, tempfile)
         if result:
             gw.show_message("congratulations! You guessed the correct answer!", "limegreen")
             gw._listeners.clear()
+            score.clear_grid()
+            score.parse()
+            score.close()
             return
         else:
             if not_word:
-                for i in range(5):
-                    gw.set_square_letter(gw.get_current_row(), N_COLS-MN, "")
-                    MN -= 1
+                for i in range(N_COLS):
+                    gw.set_square_letter(gw.get_current_row(), i, "")
                 gw.set_current_row(gw.get_current_row())
             elif gw.get_current_row() == N_ROWS-1:
                 gw.show_message(f"Ran out of tries. The answer was: {answer}", "red")
--- utils.py
+++ utils.py
@@ -4,23 +4,29 @@
 """Utility for wordle program"""
 
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR, N_COLS, WordleGWindow
+from WordleGraphics import CORRECT_COLOR, PRESENT_COLOR, MISSING_COLOR, UNKNOWN_COLOR, N_COLS, N_ROWS, WordleGWindow
 from random import choice
-from typing import Type
+from typing import Type, Union, Optional
 from copy import deepcopy
+from tempfile import NamedTemporaryFile
+from os import getcwd, unlink
 
 __all__ = [
     "choose_word",
-    "validate_responce"
+    "validate_responce",
+    "ScoreFileParser"
 ]
 
 # CONSTANT
 MINUS_COL = 5
 
+t = None
+
 # Functions
 def choose_word() -> str:
     """Chooses the answer from a list of words of five characters"""
     a = choice(FIVE_LETTER_WORDS)
+    print(a)
     return a
 
 # -----------------------------------------------
@@ -28,49 +34,107 @@ def choose_word() -> str:
 def _set_key_color_or_not(gw, key_colored, k, c, override_check=False):
     if not key_colored or override_check:
         gw.set_key_color(k.capitalize(), c)
+    
+def _add_color(gw, column, keycolored, character, color, ac, oc: Optional[bool] = None):
+    gw.set_square_color(gw.get_current_row(), column, color)
+    if oc:
+        _set_key_color_or_not(gw, keycolored, character, color, oc)
+    else:
+        _set_key_color_or_not(gw, keycolored, character, color)
+    a_copy = ac.replace(character, "", 1)
+    return a_copy
 
-def validate_responce(gw: Type[WordleGWindow], res: str, a: str) -> bool:
+def add_tempfile() -> NamedTemporaryFile:
+    """creates score file"""
+    global t
+    if not t:
+        t = NamedTemporaryFile("w+", encoding="utf-8", prefix="wordle_", dir=getcwd(), delete=False)
+    return t
+
+def validate_responce(gw: Type[WordleGWindow], res: str, a: str) -> Union[bool, bool, NamedTemporaryFile]:
     """Validates user response
     :param gw: Main Wordle window class
     :param res: User responce
     :param a: answer to the wordle
 
     Returns:
-        validity AND word-validation
+        validity | word-validation | score tempfile
     """
     global MINUS_COL
     a_copy = deepcopy(a)
     correct_counter = 0
 
+    temp = add_tempfile()
     # checks if word is not in the word list
     if res not in FIVE_LETTER_WORDS:
         gw.show_message(f"{res} is not a word!!!")
-        return False, True
+        return False, True, temp
 
     for c in a:
         col = N_COLS - MINUS_COL
         ch = gw.get_square_letter(gw.get_current_row(), col).lower()
         key_colored = gw.get_key_color(c.capitalize()) != UNKNOWN_COLOR
         if ch == c:
-            gw.set_square_color(gw.get_current_row(), col, CORRECT_COLOR)
-            _set_key_color_or_not(gw, key_colored, ch, CORRECT_COLOR, True)
-            a_copy = a_copy.replace(ch, "", 1)
+            a_copy = _add_color(gw, col, key_colored, ch, CORRECT_COLOR, a_copy, True)
             correct_counter += 1
         elif ch in a_copy:
-            gw.set_square_color(gw.get_current_row(), col, PRESENT_COLOR)
-            _set_key_color_or_not(gw, key_colored, ch, PRESENT_COLOR)
-            a_copy = a_copy.replace(ch, "", 1)
+            a_copy = _add_color(gw, col, key_colored, ch, PRESENT_COLOR, a_copy)
         else:
-            gw.set_square_color(gw.get_current_row(), col, MISSING_COLOR)
-            _set_key_color_or_not(gw, key_colored, ch, MISSING_COLOR)
-            a_copy = a_copy.replace(ch, "", 1)
+            a_copy = _add_color(gw, col, key_colored, ch, MISSING_COLOR, a_copy)
         MINUS_COL -= 1
+    
+    line = f"{gw.get_current_row()}|{correct_counter}\n"
+    temp.write(line)
+    temp.flush()
     
     MINUS_COL = 5
 
     if correct_counter == 5:
-        return True, False
-    return False, False
+        return True, False, temp
+    return False, False, temp
 
 
+class ScoreFileParser:
+    """
+    Parses and adds score to wordle grid based on the scorefile
+    """
+    cleared = False
 
+    def __init__(self, gw: Type[WordleGWindow], tmp: Type[NamedTemporaryFile]):
+        self.gw = gw
+        self.tmpfile = tmp
+
+    def parse(self):
+        """Main function to parse the score file"""
+        self.tmpfile.seek(0)
+        lines = self.tmpfile.readlines()
+        if not self.cleared:
+            self.clear_grid()
+            self.parse()
+        for l in lines:
+            row = l.split("|")[0]
+            correct_points = l.split("|")[1].replace("\n", "")
+            self.gw.set_square_letter(int(row), 0, str(int(row) + 1))
+            self.gw.set_square_letter(int(row), 4, correct_points)
+            self.gw.set_square_color(int(row), 0, PRESENT_COLOR)
+            if int(correct_points) == 5:
+                self.gw.set_square_color(int(row), 4, CORRECT_COLOR)
+            else:
+                self.gw.set_square_color(int(row), 4, MISSING_COLOR)
+        self.gw.show_message("rows                           points", "limegreen")
+
+    def clear_grid(self):
+        """Clear wordle grid"""
+        for i in range(N_ROWS):
+            self.gw.set_current_row(i)
+            for j in range(N_COLS):
+                self.gw.set_square_letter(i, j, "")
+        self.cleared = True
+
+
+    def close(self):
+        """closes the score file"""
+        self.tmpfile.close()
+        path = self.tmpfile.name
+        print(path)
+        unlink(path)
